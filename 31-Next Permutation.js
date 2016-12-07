50d5bdf66929454bcaf5b8d9e2f7382ead4ec5b1
yinancx <mockibird@live.cn>
2016-12-07 19:27:32 +0800

    Medium. Seems nothing to comment...

--- 31-Next Permutation.js
+++ 31-Next Permutation.js
@@ -11,7 +11,7 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 3,2,1 → 1,2,3
 1,1,5 → 1,5,1
 */
-
+ 
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
